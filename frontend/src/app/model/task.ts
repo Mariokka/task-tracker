8e436846be158508b23cee9ad1d7c83175169b14
Maria Okatova <mariia.okatova@volkswagen.de>
2023-08-23 17:53:20 +0200

    add default dueDate and description

--- frontend/src/app/model/task.ts
+++ frontend/src/app/model/task.ts
@@ -1,9 +1,11 @@
 export class Task {
-  static counter: number = 0;
+  static counter: number = 1;
   title: string | undefined;
-  description: string | undefined;
+  // description: string | undefined;
+  description: string = 'No rush';
   id: number | undefined;
-  dueDate: string | undefined;
+  // dueDate: string | undefined;
+  dueDate: string = "2023-09-01";
 
   constructor(title: string) {
     this.title = title;
